56e78f75df6587a6c5c733ebb9c2fbaa38f0e60c
Nguyentatho <nguyentatho14106@gmail.com>
2025-11-18 12:05:17 +0700

    'first'

--- src/pages/Layout/LayoutMain.tsx
+++ src/pages/Layout/LayoutMain.tsx
@@ -1,13 +1,8 @@
-import { useEffect } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { Outlet, useNavigate } from 'react-router'
+import { Outlet } from 'react-router'
 
 export const LayoutMain = () => {
-  const navigate = useNavigate()
-  useEffect(() => {
-      navigate('tourdetail')
-  }, [])
   return (
     <>
     <Header/>
