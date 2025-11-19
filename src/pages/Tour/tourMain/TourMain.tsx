4d908a7c09c9b5115921bd5fda63591b1a1b90df
lethinh <lephuthinh387@gmail.com>
2025-11-19 19:42:39 +0700

    update reponsive TourMain

--- src/pages/Tour/tourMain/TourMain.tsx
+++ src/pages/Tour/tourMain/TourMain.tsx
@@ -6,14 +6,14 @@ import SearchLocation from "./components/SearchLocation";
 
 export const TourMain = () => {
     return (
-        <div className="mx-auto w-[1200px] ">
+        <div className="mx-auto w-full max-w-[1200px] px-2 md:px-6">
             <div className="mt-10 flex flex-col items-center justify-center w-full">
-                <div className="w-full flex gap-6 items-start relative ">
-                    <div className="flex flex-col gap-5 w-[320px] min-w-[280px] sticky top-10">
+                <div className="w-full flex flex-col lg:flex-row gap-6 items-start">
+                    <div className="flex flex-col gap-5 w-full lg:w-[320px] min-w-[280px] mb-6 lg:mb-0">
                         <FilterPrice />
                         <FilterTour />
                     </div>
-                    <div className="flex-1 flex flex-col gap-5">
+                    <div className="flex-1 flex flex-col gap-5 sticky top-4">
                         <SearchLocation />
                         <SortComponent />
                         <ListCard />
