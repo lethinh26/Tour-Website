63eee1ad251740b4986e75e35acceea973233eb8
lethinh <lephuthinh387@gmail.com>
2025-11-29 17:16:43 +0700

    fix redux

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,14 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { tourReducer } from "./slides/tour.slide";
 
-const RootReducer = {
+const RootReducer = combineReducers({
     dataReducer : tourReducer
-};
+});
 
 export const store = configureStore({
     reducer: RootReducer
 });
 
 export type StoreType = ReturnType<typeof RootReducer>;
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
